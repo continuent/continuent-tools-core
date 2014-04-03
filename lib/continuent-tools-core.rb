7464fc895630ea0c291269ff2ca8fa9adb252092
Jeff Mace <jeffmace@gmail.com>
2014-04-02 17:57:37 -0700

    Add a small tungsten_create_load to create simple load through MySQL or the connector if available

--- lib/continuent-tools-core.rb
+++ lib/continuent-tools-core.rb
@@ -38,6 +38,101 @@ class TungstenUtil
       forward_cmd_results?(original_fwd_state)
     end
   end
+  
+  # A wrapper for running another Tungsten script on a remote host. This will 
+  # automatically forward messages to the console and add any TungstenScript 
+  # options to the command.
+  def tungsten_ssh_result(command, host, user)
+    original_fwd_state = forward_cmd_results?()
+    begin
+      if TI
+        prefix = "export CONTINUENT_ROOT=#{TI.root()}; "
+      else
+        prefix = ""
+      end
+      
+      forward_cmd_results?(true)
+      return ssh_result("#{prefix}#{command} #{get_tungsten_command_options()}", host, user)
+    ensure
+      forward_cmd_results?(original_fwd_state)
+    end
+  end
+  
+  # Run the {command} and return a string of STDOUT
+  def cmd(command, ignore_fail = false, stdin_method = nil, stdout_method = nil, stderr_method = nil)
+    errors = ""
+    result = ""
+    threads = []
+    
+    debug("Execute `#{command}`")
+    status = Open4::popen4("export LANG=en_US; #{command}") do |pid, stdin, stdout, stderr|
+      if stdin_method != nil
+        threads << Thread.new{
+          stdin_method.call(stdin)
+          stdin.close
+        }
+      else
+        stdin.close
+      end
+      
+      threads << Thread.new{
+        while data = stdout.gets()
+          if data.to_s() != ""
+            result+=data
+            
+            if data != "" && forward_cmd_results?()
+              write(data, (parse_log_level(data) || get_forward_log_level()), nil, false)
+            end
+            
+            if stdout_method != nil
+              stdout_method.call(data)
+            end
+          end
+        end
+      }
+      threads << Thread.new{
+        while edata = stderr.gets()
+          if edata.to_s() != ""
+            errors+=edata
+            
+            if edata != "" && forward_cmd_results?()
+              write(edata, (parse_log_level(edata) || get_forward_log_level()), nil, false)
+            end
+
+            if stderr_method != nil
+              stderr_method.call(edata)
+            end
+          end
+        end
+      }
+      
+      threads.each{|t| t.join() }
+    end
+    
+    result.strip!()
+    errors.strip!()
+    
+    original_errors = errors
+    rc = status.exitstatus
+    if errors == ""
+      errors = "No Errors"
+    else
+      errors = "Errors: #{errors}"
+    end
+
+    if log_cmd_results?()
+      debug("RC: #{rc}, Result: #{result}, #{errors}")
+    elsif forward_cmd_results?()
+      debug("RC: #{rc}, Result length #{result.length}, Errors length #{original_errors.length}")
+    else
+      debug("RC: #{rc}, Result length #{result.length}, #{errors}")
+    end
+    if rc != 0 && ! ignore_fail
+      raise CommandError.new(command, rc, result, original_errors)
+    end
+
+    return result
+  end
 end
 
 module TungstenScript
@@ -56,5 +151,22 @@ module TungstenScript
         end
       end
     }
+  end
+end
+
+class Array
+  def peach(&block)
+    pids = []
+    results = []
+    idx = 0
+
+    self.each{
+      |i|
+      pids[idx] = fork {
+        block.call(i)
+      }
+      idx = idx+1
+    }
+    pids.each{|pid| Process.waitpid(pid) }
   end
 end
